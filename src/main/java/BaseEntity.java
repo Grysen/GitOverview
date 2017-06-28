10d472a860268004e7af9e848cce52c30e142bdc
Sergey Gryshchuck <grysen1988gmail.com>
2017-06-28 17:28:19 +0300

    addee new class BaseEntity

---
+++ src/main/java/BaseEntity.java
@@ -0,0 +1,26 @@
+/**
+ * Created by sergey on 28.06.17.
+ */
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+    public boolean isNew(){
+    return (this.id==null);
+    }
+}
+
+
