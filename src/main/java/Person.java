c8ff2df261d65b1e3d24bdba446322d4bc22eddd
Sergey Gryshchuck <grysen1988gmail.com>
2017-06-28 17:52:26 +0300

    adding class Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,23 @@
 /**
  * Created by sergey on 28.06.17.
  */
-public class Person {
+public class Person extends BaseEntity{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +"id= "+super.getId()+
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
